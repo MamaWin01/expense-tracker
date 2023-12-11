22c9992b9df7d8f9ca427a3d59e8c323eaf626d9
MamaWin01 <63904924+MamaWin01@users.noreply.github.com>
2023-12-11 12:27:36 +0700

    change mongopassword

--- lib/Helper/constant.dart
+++ lib/Helper/constant.dart
@@ -1,4 +1,4 @@
 const MONGO_CONN_URL =
-    "mongodb+srv://mongodbAdmin445:m12345@database.jfljzdj.mongodb.net/";
+    "mongodb+srv://<datausername>:<password>@database.jfljzdj.mongodb.net/";
 const USER_COLLECTION = "user";
 const USER_EWALLET = "ewallet";
